d83df2b8e80454fd9cf552221fc60e2a6b3fa772
jason zheng <101104044@nccu.edu.tw>
2019-10-10 16:12:21 +0800

    add service detail and modify controller

--- src/document/document.controller.ts
+++ src/document/document.controller.ts
@@ -6,54 +6,43 @@ export class DocumentController {
     constructor(
         private documentService: DocumentService,
     ) { }
-    data = [
-        {
-            name: '存有與虛無',
-            author: '沙特',
-            id: 1
-        },
-        {
-            name: '單子論',
-            author: '萊布尼茲',
-            id: 2
-        },
-        {
-            name: '物不遷論',
-            author: '僧肇',
-            id: 3
-        },
-        {
-            name: '道德經',
-            author: '老子',
-            id: 4
-        }
-    ]
     @Get()
     getAllDocuments(@Response() res) {
-        res.status(HttpStatus.OK).json(this.data);
+        this.documentService.getDocumentData().subscribe(data => {
+            res.status(HttpStatus.OK).json(data);
+        })
     }
 
 
     @Get('/:id')
     getDocument(@Param() param, @Response() res) {
-        let resData = this.data.filter(data => data.id === Number(param.id))
-        res.status(HttpStatus.OK).json(resData);
+        this.documentService.getDocumentData().subscribe(data => {
+            let resData = data.filter(data => data.id === Number(param.id))
+            res.status(HttpStatus.OK).json(resData);
+        })
     }
     @Post()
     addDocument(@Response() res, @Body() document) {
-        // 做些什麼
-        res.status(HttpStatus.CREATED).json(document);
+        this.documentService.getDocumentData().subscribe(data => {
+            // 做些什麼
+            res.status(HttpStatus.CREATED).json(document);
+        })
+
     }
 
     @Patch('/:id')
     updateDocument(@Param() params, @Response() res, @Body() document) {
-        // 做些什麼
-        res.status(HttpStatus.OK).json(document);
+        this.documentService.getDocumentData().subscribe(data => {
+            // 做些什麼
+            res.status(HttpStatus.CREATED).json(document);
+        })
     }
 
     @Delete('/:id')
     deleteDocument(@Param() param, @Response() res) {
-        // 做些什麼
-        res.status(HttpStatus.OK).json(document);
+        this.documentService.getDocumentData().subscribe(data => {
+            // 做些什麼
+            res.status(HttpStatus.CREATED).json(document);
+        })
     }
 }
--- src/document/document.service.ts
+++ src/document/document.service.ts
@@ -1,4 +1,32 @@
 import { Injectable } from '@nestjs/common';
+import { of } from 'rxjs';
 
 @Injectable()
-export class DocumentService {}
+export class DocumentService {
+    data = [
+        {
+            name: '存有與虛無',
+            author: '沙特',
+            id: 1
+        },
+        {
+            name: '單子論',
+            author: '萊布尼茲',
+            id: 2
+        },
+        {
+            name: '物不遷論',
+            author: '僧肇',
+            id: 3
+        },
+        {
+            name: '道德經',
+            author: '老子',
+            id: 4
+        }
+    ]
+
+    getDocumentData(){
+        return of(this.data)
+    }
+}
