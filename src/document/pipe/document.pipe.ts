f921f0bf18e142a98a9829da71c2b91a956ec4c2
jason <tso1158687@gmail.com>
2019-10-11 12:05:23 +0800

    modify pipe

--- src/document/pipe/document.pipe.ts
+++ src/document/pipe/document.pipe.ts
@@ -1,8 +1,24 @@
-import { ArgumentMetadata, Injectable, PipeTransform } from '@nestjs/common';
+import { ArgumentMetadata, Injectable, PipeTransform, BadRequestException  } from '@nestjs/common';
+import { validate } from 'class-validator';
+import { plainToClass } from 'class-transformer';
 
 @Injectable()
 export class DocumentPipe implements PipeTransform {
-  transform(value: any, metadata: ArgumentMetadata) {
+  async transform(value, metadata: ArgumentMetadata) {
+    const { metatype } = metadata;
+    if (!metatype || !this.toValidate(metatype)) {
+        return value;
+    }
+    const object = plainToClass(metatype, value);
+    const errors = await validate(object);
+    if (errors.length > 0) {
+        throw new BadRequestException('資料格式錯誤');
+    }
     return value;
+  }
+
+  private toValidate(metatype): boolean {
+    const types = [String, Boolean, Number, Array, Object];
+    return !types.find((type) => metatype === type);
   }
 }
