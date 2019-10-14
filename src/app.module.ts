bd2c6541e8fe5ca23f0d23bca9b7787ba6306c5d
jason zheng <101104044@nccu.edu.tw>
2019-10-14 23:22:16 +0800

    set for middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,20 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DocumentController } from './document/document.controller';
 import { DocumentService } from './document/document.service';
+import { LogMiddleware } from './log.middleware';
 
 @Module({
   imports: [],
   controllers: [AppController, DocumentController],
   providers: [AppService, DocumentService],
 })
-export class AppModule {}
+
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+    .apply(LogMiddleware)
+    .forRoutes(DocumentController);
+  }
+}
--- src/log.middleware.ts
+++ src/log.middleware.ts
@@ -3,6 +3,7 @@ import { Injectable, NestMiddleware } from '@nestjs/common';
 @Injectable()
 export class LogMiddleware implements NestMiddleware {
   use(req: any, res: any, next: () => void) {
+    console.log('通過中介層')
     next();
   }
 }
